a13073d9f55c961853742f79e37635f4f5f14d03
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-03-17 19:34:30 +0000

    More licence header nonsense

--- eventbus-test-jar/src/main/java/module-info.java
+++ eventbus-test-jar/src/main/java/module-info.java
@@ -2,7 +2,6 @@
  * Copyright (c) Forge Development LLC
  * SPDX-License-Identifier: LGPL-2.1-only
  */
-
 open module net.minecraftforge.eventbus.testjars {
     requires transitive net.minecraftforge.eventbus;
 
--- eventbus-test/src/test/java/module-info.java
+++ eventbus-test/src/test/java/module-info.java
@@ -2,7 +2,6 @@
  * Copyright (c) Forge Development LLC
  * SPDX-License-Identifier: LGPL-2.1-only
  */
-
 open module net.minecraftforge.eventbus.test {
     requires net.minecraftforge.eventbus;
     requires org.junit.jupiter.api;
